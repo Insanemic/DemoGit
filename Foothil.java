5437477570bae169f131f1dd02da8f573cf52f5d
insanemic <insanemic@hotmail.com>
2015-09-25 01:08:36 -0700

    The original commit of the Foothill project

---
+++ Foothil.java
@@ -0,0 +1,11 @@
+
+public class Foothil {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
+
+/*------------- Paste of run from the console window --------
+Hello World!
+-----------------------------------------------------------*/
